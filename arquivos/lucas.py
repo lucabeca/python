c982814a76b56bca72098242bb96de2e6a275df5
lucabeca <50593880+lucabeca@users.noreply.github.com>
2019-05-13 16:15:34 -0300

    Add files via upload

--- arquivos/lucas.py
+++ arquivos/lucas.py
@@ -4,16 +4,18 @@ print("Jogo de dados:")
 
 dado1 = randint(1, 6)
 dado2 = randint(1, 6)
+jogador1 = dado1 + dado2
+
 dado3 = randint(1, 6)
 dado4 = randint(1, 6)
+jogador2 = dado3 + dado4
 
 print("Dado 1:", dado1)
 print("Dado 2:", dado2)
 print("Dado 3:", dado3)
 print("Dado 4:", dado4)
-
-jogador1 = dado1 + dado2
-jogador2 = dado3 + dado4
+print("Jogador 1:", jogador1)
+print("Jogador 2:", jogador2)
 
 if jogador1 > jogador2:
     print("Jogador 1 venceu!")
