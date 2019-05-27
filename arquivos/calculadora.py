ad86c6f647270023b64104640cc01ca06191376a
lucabeca <50593880+lucabeca@users.noreply.github.com>
2019-05-27 16:13:03 -0300

    Add files via upload

---
+++ arquivos/calculadora.py
@@ -0,0 +1,14 @@
+import time
+
+print("Super calculadora gravitacional")
+
+num_1 = float(input("Digite o peso do corpo 1:"))
+num_2 = float(input("Digite o peso do corpo 2:"))
+num_3 = float(input("Digite a distância entre os corpos:"))
+
+print("Fazendo cálculos avançados...")
+time.sleep(1)
+print("Fazendo cálculos avançados...")
+time.sleep(1)
+
+print("O resultado é", 0.000000000667 * num_1 * num_2 / num_3 ** 2, "N")
